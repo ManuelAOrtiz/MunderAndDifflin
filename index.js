d058c16d28451c2d1de126120343e6923b602714
ManuelAOrtiz <devolthdd@hotmail.com>
2018-10-01 16:55:40 -0400

    tablet responsive done

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ var dwightUrl = 'images/dwightSchrute2.jpg';
 
 var employeeText = document.getElementById('employeeText');
 var employeeImage = document.getElementById('employeeImage');
+employeeImage.style.width = '100%';
+employeeImage.style.height = '100%';
 var employeeTextH1 = document.getElementById('employeeTextH1');
 
 var galleryTarget = document.getElementById('galleryTarget');
@@ -102,25 +104,23 @@ dwight.addEventListener('mouseover',function(){
 
 
 function jimClick(screenWidth){
-	if(screenWidth >= 426 ||screenWidth <= 768){
-	employeeImage.style.width = '422px';
-	employeeImage.style.height = '300px';
+	if(screenWidth >= 426 &&screenWidth <= 768){
 	employeeTextH1.style.fontSize = '2em';
 	employeeText.style.fontSize = '1em';
-	employeeText.innerHTML = "Jim Halpert was born on October 1, 1978, to Gerald and Betsy Halpert. He has two brothers, Pete and Tom, who share his general love of pranks but have come across as unpleasant and somewhat bullying towards their little brother. Jim's parents presumably live in Scranton as well, as well as his sister Larissa and best friend Alan Murphy, who are listed as his emergency contacts at Dunder Mifflin. While Pete and Tom have been seen on the show a few times,"
-	console.log(screenWidth);
+	employeeWrapper.style.gridTemplateColumns = '.8fr 1fr'
+	employeeText.innerHTML = "Jim Halpert was born on October 1, 1978, to Gerald and Betsy Halpert. He has two brothers, Pete and Tom, who share his general love of pranks but have come across as unpleasant and somewhat bullying towards their little brother. Jim's parents presumably live in Scranton as well, as well as his sister Larissa and best friend Alan Murphy, who are listed as his emergency contacts at Dunder Mifflin. While Pete and Tom have been seen on the show a few times,";
+	employeeWrapper.style.gridTemplateRows = '20vh 50vh 1fr';
+	employeeText.style.marginTop = '65%';
 	}else if(screenWidth < 425){
-	employeeImage.style.width = '422px';
-	employeeImage.style.height = '300px';
 	employeeTextH1.style.fontSize = '3em';
 	}
 	else{
-	employeeImage.style.width = '622px';
-	employeeImage.style.height = '500px';
-	// employeeTextH1.style.fontSize = '3em';
-	employeeText.innerHTML = "Jim Halpert was born on October 1, 1978, to Gerald and Betsy Halpert. He has two brothers, Pete and Tom, who share his general love of pranks but have come across as unpleasant and somewhat bullying towards their little brother. Jim's parents presumably live in Scranton as well, as well as his sister Larissa and best friend Alan Murphy, who are listed as his emergency contacts at Dunder Mifflin. While Pete and Tom have been seen on the show a few times,"	
-	employeeText.style.fontSize = '0.9em';
-
+	employeeTextH1.style.fontSize = '3em';
+	employeeText.innerHTML = "Jim Halpert was born on October 1, 1978, to Gerald and Betsy Halpert. He has two brothers, Pete and Tom, who share his general love of pranks but have come across as unpleasant and somewhat bullying towards their little brother. Jim's parents presumably live in Scranton as well, as well as his sister Larissa and best friend Alan Murphy, who are listed as his emergency contacts at Dunder Mifflin. While Pete and Tom have been seen on the show a few times, Larissa has not. He has a niece, Vanessa, and a nephew old enough to play T-ball. In 'Branch Wars', while he is not specific on which brother he is referring to, he states that his brother's wife just had another baby. Jim enjoys cycling, and, as he reveals in a deleted scene from 'Take Your Daughter to Work Day', at times, will babysit Toby Flenderson's daughter, Sasha. He also lists soft-shell crab as his favorite food. Jim has noted that basketball 'Was kind of [his] thing' in high school. Although no high school is specifically named by Jim, both Dunmore High School (as evidenced in a deleted scene from 'Product Recall' where, during a business visit, he asks a high school student about a former teacher) and West Scranton High School (as evidenced when, during 'Email Surveillance', Pam holds up his yearbook displaying their mascot name, Invaders, across the front) are referenced in separate episodes. He attended the University of Scranton, as seen in the episode 'Basketball.'' He is wearing a University of Scranton t-shirt during the game.";	employeeText.style.fontSize = '1em';
+	employeeText.style.fontSize = '.95em';
+	employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh';
+	employeeWrapper.style.gridTemplateColumns = '1fr .8fr';
+	employeeText.style.marginTop = '30%';
 	}
 	employeeImage.style.backgroundImage = 'url('+jimUrl+')';
 	employeeImage.style.backgroundSize = '100% 100%';
@@ -131,8 +131,8 @@ function jimClick(screenWidth){
 	
 	employeeWrapper.style.backgroundSize = 0;
 	employeeWrapper.style.display = 'grid';
-	employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
-	employeeWrapper.style.gridTemplateColumns = '1fr .7fr'
+	// employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
+	// employeeWrapper.style.gridTemplateColumns = '1fr .8fr'
 
 	employeeTextH1.innerHTML = "Jim Halpert";
 	employeeTextH1.style.textAlign = 'center';
@@ -144,27 +144,23 @@ function jimClick(screenWidth){
 	employeeText.style.gridColumn = '1/2';
 	employeeText.style.gridRow  = '1/3';
 	employeeText.style.fontWeight = 'bold';
-	employeeText.style.marginTop = '30%';
+	// employeeText.style.marginTop = '30%';
 	
 }
 
 function mikeClick(screenWidth){
-	if(screenWidth >= 426 ||screenWidth <= 768){
-	employeeImage.style.width = '322px';
-	employeeImage.style.height = '270px';
+	if(screenWidth >= 426 &&screenWidth <= 768){
 	employeeTextH1.style.fontSize = '2em';
 	employeeText.style.fontSize = '1em';
 	employeeText.style.marginTop = '60%';
-	employeeWrapper.style.gridTemplateRows = '20vh .6fr 15vh'
+	employeeWrapper.style.gridTemplateRows = '20vh 50vh 1fr';
 	employeeText.innerHTML = 'Michael Gary Scott was born March 15, 1964 in Scranton, Pennsylvania. He came from a relatively difficult childhood of loneliness. In "Diversity Day", Michael claims to be of English, Irish, German and Scottish ancestry. He also claims to be two-fifteenths Native American. He has mentioned a stepfather, Jeff, whom he despises. In "Nepotism", it is revealed that Michael had a half-sister, from whom he was estranged for 15 years.'	
 	}else if(screenWidth < 426){
 
 	}
 	else{
-	employeeImage.style.width = '622px';
-	employeeImage.style.height = '500px';
 	employeeTextH1.style.fontSize = '2em';
-	employeeText.style.fontSize = '1em';
+	employeeText.style.fontSize = '1.1em';
 	employeeText.style.marginTop = '30%';
 	employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
 	employeeText.innerHTML = 'Michael Gary Scott was born March 15, 1964 in Scranton, Pennsylvania. He came from a relatively difficult childhood of loneliness. In "Diversity Day", Michael claims to be of English, Irish, German and Scottish ancestry. He also claims to be two-fifteenths Native American. He has mentioned a stepfather, Jeff, whom he despises. In "Nepotism", it is revealed that Michael had a half-sister, from whom he was estranged for 15 years. As a consequence of their reunion, Michael hires his nephew Luke as an intern for the office, but eventually confronts the incompetent and rude Luke and ends up spanking him in front of the office, leading Luke to burst into tears and quit. In the episode "Take Your Daughter to Work Day", Michael makes the claim that he was a child star on a kids show called Fundle Bundle; however, it becomes clear that he simply appeared on the show as one of many guest children. In the old recording being played, he speaks touchingly about what he wanted when he grew up: get married, have "100 kids" so he could then have "100 friends" and none of them could say no to being his friend. Michael did not attend college, having lost all his tuition money in a pyramid scheme.'
@@ -186,7 +182,7 @@ function mikeClick(screenWidth){
 	employeeTextH1.style.gridColumn = '1/2';
 
 	// employeeText.innerHTML = 'Michael Gary Scott was born March 15, 1964 in Scranton, Pennsylvania. He came from a relatively difficult childhood of loneliness. In "Diversity Day", Michael claims to be of English, Irish, German and Scottish ancestry. He also claims to be two-fifteenths Native American. He has mentioned a stepfather, Jeff, whom he despises. In "Nepotism", it is revealed that Michael had a half-sister, from whom he was estranged for 15 years. As a consequence of their reunion, Michael hires his nephew Luke as an intern for the office, but eventually confronts the incompetent and rude Luke and ends up spanking him in front of the office, leading Luke to burst into tears and quit. In the episode "Take Your Daughter to Work Day", Michael makes the claim that he was a child star on a kids show called Fundle Bundle; however, it becomes clear that he simply appeared on the show as one of many guest children. In the old recording being played, he speaks touchingly about what he wanted when he grew up: get married, have "100 kids" so he could then have "100 friends" and none of them could say no to being his friend. Michael did not attend college, having lost all his tuition money in a pyramid scheme.'
-	employeeText.style.fontSize = '0.9em';
+	// employeeText.style.fontSize = '0.9em';
 	employeeText.style.gridColumn = '2/3';
 	employeeText.style.gridRow  = '1/3';
 	employeeText.style.fontWeight = 'bold';
@@ -194,18 +190,18 @@ function mikeClick(screenWidth){
 }
 
 function pamClick(screenWidth){
-	if(screenWidth >= 426 ||screenWidth <= 768){
-	employeeImage.style.width = '322px';
-	employeeImage.style.height = '280px';
-	employeeWrapper.style.gridTemplateRows = '13vh .6fr 15vh'
-
+	if(screenWidth >= 426 &&screenWidth <= 768){
+	employeeWrapper.style.gridTemplateRows = '20vh 50vh 1fr';
+	employeeText.style.marginTop = '45%';
+	employeeWrapper.style.gridTemplateColumns = '.9fr 1fr';
+	employeeTextH1.style.fontSize = '2em';
 	}else if(screenWidth < 426){
 
 	}
 	else{
-	employeeImage.style.width = '622px';
-	employeeImage.style.height = '500px';
 	employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
+	employeeText.style.margin = '30% 1%';
+	employeeTextH1.style.fontSize = '3em';
 	}
 	employeeImage.style.backgroundImage = 'url('+pamUrl+')';
 	employeeImage.style.backgroundSize = '100% 100%';
@@ -216,11 +212,11 @@ function pamClick(screenWidth){
 	
 	employeeWrapper.style.backgroundSize = 0;
 	employeeWrapper.style.display = 'grid';
-	employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
-	employeeWrapper.style.gridTemplateColumns = '1fr .7fr'
+	// employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
+	// employeeWrapper.style.gridTemplateColumns = '1fr .8fr'
 
 	employeeTextH1.innerHTML = "Pam Halpert";
-	employeeTextH1.style.fontSize = '3em';
+	// employeeTextH1.style.fontSize = '3em';
 	employeeTextH1.style.gridRow = '3/4';
 	employeeTextH1.style.gridColumn = '2/3';
 	employeeTextH1.textAlign = 'center'
@@ -230,18 +226,22 @@ function pamClick(screenWidth){
 	employeeText.style.gridColumn = '1/2';
 	employeeText.style.gridRow  = '1/4';
 	employeeText.style.fontWeight = 'bold';
-	employeeText.style.margin = '30% 1%';
+	// employeeText.style.margin = '30% 1%';
 }
 function dwightClick(screenWidth){
-	if(screenWidth >= 426 ||screenWidth <= 768){
-	employeeImage.style.width = '322px';
-	employeeImage.style.height = '300px';
+	if(screenWidth >= 426 &&screenWidth <= 768){
+	employeeWrapper.style.gridTemplateRows = '20vh 50vh 1fr';
+	employeeText.style.marginTop = '45%';
+	employeeWrapper.style.gridTemplateColumns = '.9fr 1fr';
+	employeeTextH1.style.fontSize = '2em';
 	}else if(screenWidth < 426){
 
 	}else{
-	employeeImage.style.width = '622px';
-	employeeImage.style.height = '500px';
+	employeeTextH1.style.fontSize = '3em';
+	employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
+	employeeWrapper.style.gridTemplateColumns = '.7fr 1fr'
 	}
+
 	employeeImage.style.backgroundImage = 'url('+dwightUrl+')';
 	employeeImage.style.backgroundSize = '100% 100%';
 	employeeImage.style.gridColumn = '1/2';
@@ -249,12 +249,11 @@ function dwightClick(screenWidth){
 	
 	employeeWrapper.style.backgroundSize = 0;
 	employeeWrapper.style.display = 'grid';
-	employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
-	employeeWrapper.style.gridTemplateColumns = '.7fr 1fr'
+	// employeeWrapper.style.gridTemplateRows = '13vh .8fr 15vh'
+	// employeeWrapper.style.gridTemplateColumns = '.7fr 1fr'
 
 	employeeTextH1.innerHTML = "Dwight Schrute";
 	employeeTextH1.style.textAlign = 'center';
-	employeeTextH1.style.fontSize = '3em';
 	employeeTextH1.style.gridRow = '3/4';
 	employeeTextH1.style.gridColumn = '1/2';
 
